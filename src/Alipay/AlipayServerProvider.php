590a6da01f5d6e23cd65dd8b01664f8def6522a7
root <root@localhost.localdomain>
2017-05-04 16:02:18 +0800

    删除多余

--- src/Alipay/AlipayServerProvider.php
+++ src/Alipay/AlipayServerProvider.php
@@ -13,35 +13,8 @@ class AlipayServiceProvider extends ServiceProvider
 	 */
 	public function boot()
 	{
-		//$this->setupConfig();
 	}
 
-	/**
-	 * Setup the config.
-	 *
-	 * @return void
-	 */
-	protected function setupConfig()
-	{
-		$source_config = realpath(__DIR__ . '/../../config/config.php');
-		$source_mobile = realpath(__DIR__ . '/../../config/mobile.php');
-		$source_web = realpath(__DIR__ . '/../../config/web.php');
-		if ($this->app instanceof LaravelApplication && $this->app->runningInConsole()) {
-			$this->publishes([
-				$source_config => config_path('latrell-alipay.php'),
-				$source_mobile => config_path('latrell-alipay-mobile.php'),
-				$source_web => config_path('latrell-alipay-web.php'),
-			]);
-		} elseif ($this->app instanceof LumenApplication) {
-			$this->app->configure('latrell-alipay');
-			$this->app->configure('latrell-alipay-mobile');
-			$this->app->configure('latrell-alipay-web');
-		}
-		
-		$this->mergeConfigFrom($source_config, 'latrell-alipay');
-		$this->mergeConfigFrom($source_mobile, 'latrell-alipay-mobile');
-		$this->mergeConfigFrom($source_web, 'latrell-alipay-web');
-	}
 
 	/**
 	 * Register the service provider.
